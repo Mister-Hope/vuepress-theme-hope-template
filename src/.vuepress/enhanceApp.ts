ca297c42a721a7b69067d634d27e38cb0aa6b913
Mr.Hope <zhangbowang1998@gmail.com>
2020-05-25 12:44:58 +0800

    update deps

--- src/.vuepress/enhanceApp.ts
+++ src/.vuepress/enhanceApp.ts
@@ -5,7 +5,7 @@
  * @LastEditTime: 2020-02-20 23:30:58
  * @Description: Vuepress增强配置
  */
-import { EnhanceApp } from 'vuepress-types';
+import { EnhanceApp } from "@mr-hope/vuepress-types";
 
 const enhanceApp: EnhanceApp = () => {};
 
